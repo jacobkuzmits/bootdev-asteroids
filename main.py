a4b4711dfaa29c4d8dce6cfcbec54d9592c6e155
Jacob Kuzmits <kuzmits@gmail.com>
2025-10-11 15:36:25 -0400

    feat: add Player class and draw it

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	clock = pygame.time.Clock()
-	dt = 0
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
-		screen.fill("black")
-		pygame.display.flip()
-		dt = clock.tick(60) / 1000
-		
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
-	main()
+    main()
